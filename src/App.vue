1a616c907edbd13f9ad5742fec0a10e4ded9bd49
torish14 <nanamin.104@icloud.com>
2022-01-09 22:57:53 +0900

    ✨ feat: App.vue で Hello Vue! を表示

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		{{ message }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	data() {
+		return {
+			message: 'Hello Vue!'
+		}
+	}
+}
+</script>
+
+<style scoped></style>
