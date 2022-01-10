bb50dd1da617431ec88087c0e39b92c9fcae2fd4
torish14 <nanamin.104@icloud.com>
2022-01-10 20:49:42 +0900

    ✨ feat: eslint の設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console':
+      process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger':
+      process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-components': 'off',
+  },
+}
